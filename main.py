3a56fe58b2b9100a56a11a745cc7478c6581c5b9
jd-galvan <jdgsuazo@gmail.com>
2023-06-19 02:04:14 -0500

    Attributes of the app

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from routes.mock import mock_router
 from config.database import engine, Base
 
 app = FastAPI()
+app.title = "Fast GPT Mock"
+app.version = "0.0.1"
+
 app.include_router(mock_router)
 Base.metadata.create_all(bind=engine)
