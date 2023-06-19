cbca02149da9f4fcd4e7e802ea009099b30e05ae
jd-galvan <jdgsuazo@gmail.com>
2023-06-19 01:27:34 -0500

    Adding constants

--- services/gpt.py
+++ services/gpt.py
@@ -1,6 +1,6 @@
 import openai
 from config.settings import settings
-from util.constants import GPT_MODEL
+from util.constants import GPT_MODEL, GPT_TEMPERATURE, GPT_MAX_TOKENS, GPT_CONTEXT_FOR_MODEL
 
 openai.api_key = settings.gpt_api_key
 
@@ -8,9 +8,9 @@ openai.api_key = settings.gpt_api_key
 def get_response(prompt: str):
     response = openai.Completion.create(
         model=GPT_MODEL,
-        prompt=f"Quiero que me respondas con objetos JSON y si es mas de una respuesta, lo respondas en un arreglo. {prompt}",
-        temperature=0.3,
-        max_tokens=500
+        prompt=f"{GPT_CONTEXT_FOR_MODEL} {prompt}",
+        temperature=GPT_TEMPERATURE,
+        max_tokens=GPT_MAX_TOKENS
     )
     result = dict(list(response["choices"])[0])["text"].strip()
     return result
--- util/constants.py
+++ util/constants.py
@@ -1,1 +1,4 @@
 GPT_MODEL = "text-davinci-003"
+GPT_TEMPERATURE = 0.3
+GPT_MAX_TOKENS = 500
+GPT_CONTEXT_FOR_MODEL = "Quiero que me respondas con objetos JSON y si es mas de una respuesta, lo respondas en un arreglo. Loas atributos deben estar entre comillas dobles."
